88ccea6e93628af106b637697eab23bfb05dcf62
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-06-08 16:36:04 +0300

    Fix punctuation issues

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
-
 class Coffee:
     def __init__(self, size, price):
         self.size = size
         self.price = price
+
     @property
     def size(self):
         return self._size
-    
+
     @size.setter
     def size(self, value):
+        value = value.lower()
+
         if value not in ['small', 'medium', 'large']:
             raise ValueError("Size must be 'small', 'medium', or 'large'")
+
         self._size = value
 
     @property
     def price(self):
         return self._price
-    
+
     @price.setter
     def price(self, value):
+        value = float(value)
+
         if value < 0:
             raise ValueError("Price cannot be negative")
+
         self._price = value
 
     def tip(self):
-        print("This coffee is great! Here is a tip!")
-        self.price += 1
-
-
+        print("This coffee is great, here’s a tip!")
+        self._price += 1
